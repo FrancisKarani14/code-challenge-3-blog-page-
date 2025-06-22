79204737e146f401d0ee4d7f8a8a6204bca07a21
Francis <franciskarani15@gmail.com>
2025-06-22 12:02:01 +0300

    creates a card for the blogs

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const authorInput = document.getElementById("author")
 const content = document.getElementById("blogContent")
 const date = document.getElementById("date")
 //  create card
+function createCard(transport) {
+    return`
+    <div style="background:rgba(245, 245, 245, 0.541):padding:24px;border:1px solid #eee;box-shadow:0 2px 4px rgba(0,0,0,0.08);margin-bottom:16px;transition:all 0.3s ease;">
+    <h3 style="font-size:1.25rem;font-weight:600;color:#000004;margin-bottom:8px;"> ${transport.nameInput}</h3>
+    <h2 style="font-size:1rem;font-weight:500;color:#000004;margin-botton:10px;">${transport.}
+    
+    `
+    
+}
