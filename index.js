251a1b369e347f0e3f59a38a0749c27aeeda8aa3
Francis <franciskarani15@gmail.com>
2025-06-22 11:36:48 +0300

    creates javascript and declares variables

---
+++ index.js
@@ -0,0 +1,8 @@
+// declare variables
+const nameInput = document.getElementById("title")
+const category = document.getElementById("category")
+const image= document.getElementById("image")
+const authorInput = document.getElementById("author")
+const content = document.getElementById("blogContent")
+const date = document.getElementById("date")
+//  create card
